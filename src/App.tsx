e27f46b8015c25f8e5ed8f6fcc7fc96e2069c2ea
Yevhenii Chunachenko <yevhenii.chumachenko@norse.digital>
2024-02-24 07:40:48 +0200

    lotttie

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
 import { useState } from "react";
 import ussStyles from "./appStyle";
-import battleAnimation from './assets/Animation - 1708668877016.gif';
+// import battleAnimation from './assets/Animation - 1708668877016.gif';
+import animationData from './assets/Animation - 1708668877016.json';
 import { Button } from '@mui/material';
+import Lottie from 'react-lottie';
 
 function App() {
   const styles = ussStyles();
-  const [isBattle, setBattle] = useState<boolean>(false);
-  const [isAnimation, setAnimation] = useState<boolean>(false);
-  const [isVictory, seIsVictory] = useState<boolean>(false);
-  const [isDefeat, setIsDefeat] = useState<boolean>(false)
+  const [isBattle, setBattle] = useState<string>(null);
+  // const [isAnimation, setAnimation] = useState<boolean>(false);
+  // const [isVictory, seIsVictory] = useState<boolean>(false);
+  // const [isDefeat, setIsDefeat] = useState<boolean>(false)
+  const [result, setResult] = useState<string>(null);
 
   const handleBattleClick = () => {
     setTimeout(() => {
-      setBattle(true);
-      setAnimation(true);
+      setBattle('start');
+      // setAnimation(true);
     }, 300)
     setTimeout(() => {
-      setAnimation(false);
+      // setAnimation(false);
+      setBattle('end');
       const random = Math.random();
       if (random > 0.5) {
-        seIsVictory(true);
+        setResult('victory');
       } else {
-        setIsDefeat(true);
+        setResult('defeat');
       }
     }, 10000)
   }
@@ -30,13 +34,23 @@ function App() {
       {!isBattle &&
         <Button className={styles.battleButton} onClick={handleBattleClick} variant={"contained"}>Start battle</Button>
       }
-      {isAnimation &&
-        <img className={styles.animation} src={battleAnimation} alt="battle"/>
-      }
-      {isVictory &&
+      {/*{(isBattle === 'start' &&*/}
+      {/*  // <img className={styles.animation} src={battleAnimation} alt="battle"/>*/}
+      <div className={styles.animation}>
+        <Lottie
+          height={300}
+          width={300}
+          options={{
+            loop: true,
+            autoplay: true,
+            animationData: animationData,
+          }}
+        />
+      </div>
+      {(result === 'victory') &&
         <h1 className={styles.result}>Victory</h1>
       }
-      {isDefeat &&
+      {(result === 'defeat') &&
         <h1 className={styles.result}>Defeat</h1>
       }
     </div>
